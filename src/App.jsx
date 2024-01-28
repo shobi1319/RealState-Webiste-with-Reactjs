38f367ba356b8db761eb4540d4270f3c4a330bbb
shobi1319 <muhammadshoaib.bscsf21@iba-suk.edu.pk>
2024-01-28 19:01:40 +0500

    half done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Header from "./components/Header/header";
 import Comp from "./components/Comp/Comp"
+import "./App.css";
 function App() {
   return (
     <div className="App">
-     
-     <Header/>
+       <div>
+     <div className="white-gradient"/>
+    <Header/>
      <Comp/>
-    
+    </div>
     </div>
     
   );
